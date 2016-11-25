fa71a0020ad64b3a87eb125c219058ef7397b608
Carlos Andres Lopez <asrael@utp.edu.co>
2016-11-25 11:18:33 -0500

    Add files via upload

--- leermuro.py
+++ leermuro.py
@@ -1,21 +1,8 @@
 import pygame
 import json
 
-ANCHO=800
-ALTO=660
-
-def Separar(l, ancho):
-    con=0
-    matriz=[]
-    linea=[]
-    for i in l:
-        linea.append(i)
-        con+=1
-        if con==ancho:
-            matriz.append(linea)
-            linea=[]
-            con=0
-    return matriz
+ANCHO=1128
+ALTO=480
 
 def Recortar(archivo, anc, alc):
     linea=[]
@@ -31,56 +18,47 @@ def Recortar(archivo, anc, alc):
 if __name__ == '__main__':
     pygame.init()
     pantalla=pygame.display.set_mode([ANCHO,ALTO])
-
     with open('mapae.json') as js_ar:
         base=json.load(js_ar)
 
-    lm=[]
-    for l in base['layers']:
-        print l['name']
-        if l['name']=='muros':
-            lm=l['data']
-
-    #print lm
-    an_fondo=base["width"]
-    mapa=Separar(lm, an_fondo)
-    for f in mapa:
-        print f
-    #print m
-
     nom_arc=''
     for l in base['tilesets']:
+        #print l['image']
         nom_arc=l['image']
         al_c=l['tileheight']
         an_c=l['tilewidth']
 
     print nom_arc, al_c, an_c
-    lin=Recortar(nom_arc, an_c, al_c)
+    lm=Recortar(nom_arc, an_c, al_c)
+    '''
+    xp=0
+    for e in lm:
+        cuadro=e
+        pantalla.blit(e,[xp,0])
+        xp+=an_c
+    '''
 
-    fondo=pygame.image.load('mapa.png').convert()
-    pantalla.blit(fondo,[0,0])
+    mapa=[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3, 3, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 7, 7, 7, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 0, 0, 6, 7, 7, 7, 7, 8, 0, 0, 7, 7, 7, 7, 7, 7]
+    ]
+
     xy=0
     for f in mapa:
         xp=0
         for c in f:
             if c!=0:
-                cuadro=lin[c-1]
-                print c, [xp,xy], an_c
+                cuadro=lm[c-1]
                 pantalla.blit(cuadro,[xp,xy])
             xp+=an_c
         xy+=al_c
 
+
     pygame.display.flip()
     fin=False
-    reloj=pygame.time.Clock()
-    var_x=0
     while not fin:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin=True
-        var_x+=-2
-        pantalla.fill((0,0,0))
-        pantalla.blit(fondo,[var_x,0])
-        
-        pygame.display.flip()
-        reloj.tick(60)
